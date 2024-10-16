212445b805ba6287d1c331f99742f9d777f80d96
GersonESS <gersones@outlook.com>
2024-10-16 08:41:50 -0300

    a

---
+++ teste.c
@@ -0,0 +1,41 @@
+/* Simple C program that connects to MySQL Database server*/
+    
+    #include <stdio.h>
+    #include <C:\conectorC\include/mysql.h>
+    main() {
+      MYSQL *conn;
+      MYSQL_RES *res;
+      MYSQL_ROW row;
+
+      char *server = "localhost";
+      char *user = "root";
+      //set the password for mysql server here
+      char *password = "*********"; /* set me first */
+      char *database = "Real_flights";
+
+      conn = mysql_init(NULL);
+
+      /* Connect to database */
+      if (!mysql_real_connect(conn, server,
+            user, password, database, 0, NULL, 0)) {
+          fprintf(stderr, "%s\n", mysql_error(conn));
+          exit(1);
+      }
+
+      /* send SQL query */
+      if (mysql_query(conn, "show tables")) {
+          fprintf(stderr, "%s\n", mysql_error(conn));
+          exit(1);
+      }
+
+      res = mysql_use_result(conn);
+
+      /* output table name */
+      printf("MySQL Tables in mysql database:\n");
+      while ((row = mysql_fetch_row(res)) != NULL)
+          printf("%s \n", row[0]);
+
+      /* close connection */
+      mysql_free_result(res);
+      mysql_close(conn);
+    }
