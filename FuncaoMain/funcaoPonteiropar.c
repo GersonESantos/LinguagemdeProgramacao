b9932363bd70699b3c04298477ced5e99ba029ec
GersonESS <gersones@outlook.com>
2024-10-17 17:08:58 -0300

    Update funcaoPonteiropar.c

--- FuncaoMain/funcaoPonteiropar.c
+++ FuncaoMain/funcaoPonteiropar.c
@@ -1,11 +1,24 @@
+//Ponteiro como parâmetro para uma função
+
 #include <stdio.h>
-void imprimir(int num) {
+/*void imprimir(int num) {
     printf("Essa e num %d\n" , num);
     num=80;
 }
 int main() {
     int idade = 20;
     imprimir(idade);
+    printf("Essa e a idade %d\n", idade);
+    return 0;
+}
+*/
+void imprimir(int *num) {
+    printf("Essa e num %d\n" , *num);
+    *num=80;
+}
+int main() {
+    int idade = 20;
+    imprimir(&idade);
     printf("Essa e a idade %d\n", idade);
     return 0;
 }
